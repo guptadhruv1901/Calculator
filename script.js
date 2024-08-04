5a95abe74a19cebb433006f48794f6d047037973
Dhruv Gupta <143761021+guptadhruv1901@users.noreply.github.com>
2024-08-04 13:27:20 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -19,3 +19,12 @@ function allClear(){
 function del(){
     outputScreen.value = outputScreen.value.slice(0,-1)
 }
+
+function changeTheme() {
+    document.body.classList.toggle('dark');
+    if (document.body.classList.contains('dark')) {
+        document.querySelector('#icon').src = 'dark theme icon/sun-regular-24(1).png';
+    }else{
+        document.querySelector('#icon').src = 'dark theme icon/moon.png';
+    }
+}
